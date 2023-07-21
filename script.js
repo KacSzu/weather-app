b6e9d15010ac77476ce9c4af767031a9f7708af2
KacSzu <szulckacper19@gmail.com>
2023-07-21 15:04:28 +0200

    implementing basic functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 /// API FROM https://www.visualcrossing.com/
-const API_KEY = "";
+const API_KEY = "YLE8ZUKY3PMKBS3EUF6CXFWPX";
 const weatherIcon = document.querySelector(".weather-icon");
 const mainContainer = document.querySelector(".container");
 const searchContainer = document.querySelector(".search-container");
@@ -15,17 +15,40 @@ const getWeatherData = async function (location) {
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}%2CUK?unitGroup=us&key=${API_KEY} `
   )
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => showWeatherModal(data))
     .catch((error) => {
       alert("Wrong location :/");
     });
 };
-const showWeatherModal = function (e, data) {
-  e.preventDefault();
-  const location = searchInput.value;
-  getWeatherData(location);
-  searchInput.value = "";
+const showWeatherModal = function (data) {
+  const address = data.address;
+  const temperature = data.currentConditions.temp;
+  const celciusTemperature = ((temperature - 32) * (5 / 9))
+  const humidity = data.currentConditions.humidity;
+  const description = data.description;
+console.log(data);
+  mainContainer.innerHTML=`<div class="weather-container">
+  <span class="location"> ${address[0].toUpperCase() + address.slice(1)}</span>
+  <span class="location-description"
+    >${description}</span
+  >
+
+  <div class="location-data">
+      <div class="temperature">
+          <ion-icon class="data-icon" name="thermometer-outline"></ion-icon>
+          <span class="data">${celciusTemperature.toFixed(1)} °C</span>
+      </div>
+      <div class="humidity">
+          <ion-icon  class="data-icon" name="umbrella-outline"></ion-icon>
+          <span class="data">${humidity} %</span>
+      </div>
+  </div>
+</div>`
 };
 
 weatherIcon.addEventListener("click", showSearchBar);
-searchContainer.addEventListener("submit", showWeatherModal);
+searchContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = searchInput.value;
+  getWeatherData(location);
+});
